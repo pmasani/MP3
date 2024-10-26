29b4cfc954e99af35d2bf1a635b89bf747bb0ce8
Audris Mockus <audris@utk.edu>
2024-10-26 17:02:56 -0400

    added github repos

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 You have  approximately 75 models and 105 datasets hosted on
 HuggingFace Hub in your netid_model and netid_data
 files.
+In addition, you have a sample of github repositories mentioned in
+scientific papers.
 
 For each please scrape and store the content of the readme files
 For each retrieved README file extract all URLs and DOI's in it. For
@@ -11,7 +13,7 @@ Store the results in a (compressed) json file containing a
 dictionary with the following keys:
 
 1. 'id': e.g., 'LoneStriker/SauerkrautLM-Mixtral-8x7B-3.0bpw-h6-exl2'
-1. 'type': '(data|model)'
+1. 'type': '(data|model|source)'
 1. 'url':
     e.g. 'https://huggingface.co/datasets/pankajmathur/alpaca_orca/raw/main/README.md'
 	or 'https://huggingface.co/iamacaru/climate/raw/main/README.md'
--- example.py
+++ example.py
@@ -5,8 +5,9 @@ import sys, gzip
 
 
 utid = 'audris'
-base= { 'model':'https://huggingface.co/', 'data': 'https://huggingface.co/datasets/' }
+base= { 'model':'https://huggingface.co/', 'data': 'https://huggingface.co/datasets/', 'source': 'https://' }
 post = '/raw/main/README.md'
+postGH = 'blob/master/README.md' # or it could be 'blob/main/README.md'
 
 extU = URLExtract()
 DOIpattern = r'\b(10\.\d{4,9}\/[-._;()/:A-Z0-9]+)\b/i'
@@ -23,13 +24,19 @@ def extractDOIs (c):
 fo = gzip.open(f"output/{utid}.json.gz", 'w')
 
 def run (tp):
+ post0 = post
  with open(f"input/{utid}_{tp}", 'r') as f:
   for line in f:
    line = line.strip ()
+   if tp == 'source':
+    (npapers,line) = line.split(';');
+    post0 = postGH
    print(line)
-   url = base[tp] + f"{line}{post}"
+   url = base[tp] + f"{line}{post0}"
    r = requests.get (url)
    content = r.text;
+   #github returns repos that do not exist, need to detect that here
+   #github when you give master instead of main, that might cause issues as well
    urls = extractURLs(content)
    dois = extractDOIs(content)
    res = { 'ID': line, 'type': tp, 'url': url, 'content': content, 'links': urls, 'dois': dois }
@@ -38,3 +45,4 @@ def run (tp):
 
 run('model')
 run('data')
+run('source')
